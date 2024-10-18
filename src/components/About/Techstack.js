bde876fca528065566da3d4104abc26afff99889
Matheus Silva <msilvaqs@outlook.com>
2024-10-18 10:37:14 -0400

    Initial commit

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiGit,
+} from "react-icons/di";
+import { FaAws } from "react-icons/fa";
+import { GrGraphQl } from "react-icons/gr";
+import { IoLogoCss3 } from "react-icons/io";
+
+import { RiNextjsLine, RiTailwindCssFill } from "react-icons/ri";
+import { SiPostgresql, SiTypescript, SiPrisma } from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTypescript />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <IoLogoCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <RiNextjsLine />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaAws />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <GrGraphQl />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPrisma />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostgresql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <RiTailwindCssFill />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
