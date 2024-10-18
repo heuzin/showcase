3929526f62dbbaeda96b66b803deb77b3e7cfa7a
Matheus Silva <msilvaqs@outlook.com>
2024-10-18 12:25:41 -0400

    Adding projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import mastermind from "../../Assets/Projects/mastermind.png";
+import talko from "../../Assets/Projects/talko.png";
+import oasis from "../../Assets/Projects/oasis.png";
+import theatio from "../../Assets/Projects/theatio.png";
+import houseMarketplace from "../../Assets/Projects/house-marketplace.png";
+import githubFinder from "../../Assets/Projects/github-finder.png";
 
 function Projects() {
   return (
@@ -23,68 +23,67 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={talko}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Talko"
+              description="Personal Chat Rooms or Workspace to share resources and hangout with friends build with Next.js, React, Zustand, Prisma, Clerk, PostgreSQL and Tailwind. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              ghLink="https://github.com/heuzin/talko"
+              demoLink="https://talko-production.up.railway.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={oasis}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Oasis"
+              description="Online marketplace for short-and-long-term homestays and experiences in various countries and regions. Build with Next.js, React, Zustand, Prisma, NextAuth, MongoDB and Tailwind. Users are able to add their properties for reservations, create reservations and like or dislike listings."
+              ghLink="https://github.com/heuzin/oasis"
+              demoLink="https://oasis-sand.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={mastermind}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Mastermind"
+              description="State-of-the-art AI SaaS Platform with five AI tools. Build with Next.js, OpenAI, ReplicateAI Stripe, Crips, Prisma and Clerk. Chatbot, Image and Code generation with OpenAI, music and video generation with ReplicateAI. Stripe for simulating user payment on limit free tier."
+              ghLink="https://github.com/heuzin/mastermind"
+              demoLink="https://mastermind-production-c347.up.railway.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={theatio}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Theatio"
+              description="Millions of movies and TV shows to discover. Build with Next.js, TypeScript, Tailwind and Themoviedb API. Find the latest and greatest movies and shows"
+              ghLink="https://github.com/heuzin/theatio"
+              demoLink="https://imdb-peach-iota.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={houseMarketplace}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="House Marketplace"
+              description="Find great deals on Houses for Sale or Rent. Build with React, Typescript and Firebase. Best online real estate platform for selling, buying and rent a property, commercial, house, villa, home, apartment."
+              ghLink="https://github.com/heuzin/house-marketplace"
+              demoLink="https://house-marketplace-ten-khaki.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={githubFinder}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="GitHub Finder"
+              description="Search Github profiles, Read full users profile details. Links to users websites. Build with React, TypeScript, Tailwind and GitHub API."
+              ghLink="https://github.com/heuzin/github-finder"
+              demoLink="https://github-finder-nu-two.vercel.app/"
             />
           </Col>
         </Row>
